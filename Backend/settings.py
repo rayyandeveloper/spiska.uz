16e2b979f9ae7642596127aa528ac89673b20d98
rayyandeveloper <rayyan.dev@mail.ru>
2022-12-08 00:45:12 +0500

    commit

--- Backend/settings.py
+++ Backend/settings.py
@@ -30,7 +30,6 @@ ALLOWED_HOSTS = []
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
 INSTALLED_APPS = [
-    'django_crontab',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
