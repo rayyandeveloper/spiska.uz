2f558255c072cb0327610df5bb4f1c279adb59a7
rayyandeveloper <rayyan.dev@mail.ru>
2022-12-09 05:55:28 +0500

    ass

--- home/views.py
+++ home/views.py
@@ -4,9 +4,24 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response  
 from .models import *
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def homepage(request):
     response = []
+
+    if request.method == "POST":
+        data = request.data.get('data')
+        
+        for i in data:
+            obj = Region.objects.create(name=i['name'])
+            for x in i['districts']:
+                Region.objects.create(name=x['name'], region=obj)
+
+        return Response({'status': 200})
+
+
+            
+    
+
 
     for i in Region.objects.all():
         t = {'name': i.name, 'districts' : [{'name' : j.name} for j in District.objects.filter(region=i)]}
