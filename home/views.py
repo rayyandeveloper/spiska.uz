d75cf41ec4af0e612fea87e8631ca3232368e46c
rayyandeveloper <rayyan.dev@mail.ru>
2022-12-09 14:15:14 +0500

    product api

--- home/views.py
+++ home/views.py
@@ -1,31 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework.decorators import api_view
-from rest_framework.response import Response  
-from .models import *
 
-@api_view(["GET", "POST"])
 def homepage(request):
-    response = []
-
-    if request.method == "POST":
-        data = request.data.get('data')
-        
-        for i in data:
-            obj = Region.objects.create(name=i['name'])
-            for x in i['districts']:
-                District.objects.create(name=x['name'], region=obj)
-
-        return Response({'status': 200})
-
-
-            
-    
-
-
-    for i in Region.objects.all():
-        t = {'name': i.name, 'districts' : [{'name' : j.name} for j in District.objects.filter(region=i)]}
-        
-        response.append(t)
-
-    return Response(response)
+    return HttpResponse('hello')
